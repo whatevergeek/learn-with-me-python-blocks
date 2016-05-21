e9ff22d53cc72b0a1ad583eb9b47516658d04cd4
whatevergeek <whatevergeek@gmail.com>
2016-05-21 12:58:31 +0800

    syncing

--- block02/exercise11.py
+++ block02/exercise11.py
@@ -1,19 +1,14 @@
 """
-Exercise 10: What Was That?
+Exercise 11: Asking Questions
 """
 
-tabby_cat = "\tI'm tabbed in."
-persian_cat = "I'm split\non a line."
-backslash_cat = "I'm \\ a \\ cat."
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
 
-fat_cat = """
-I'll do a list:
-\t* Cat food
-\t* Fishies
-\t* Catnip\n\t* Grass
-"""
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
 
-print tabby_cat
-print persian_cat
-print backslash_cat
-print fat_cat
